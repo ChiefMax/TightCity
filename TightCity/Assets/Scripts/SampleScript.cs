8a85baf22b37f3cd5251df3c85bcef2129cec77d
Max <maxmanenschijn@gmail.com>
2019-10-13 19:54:21 +0200

    Init

---
+++ TightCity/Assets/Scripts/SampleScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+/**
+ * This is a sample behaviour that can be attached to any object.
+ * Actual building behaviour is implemented through the editor.
+ * 
+ * @author J.C. Wichman / InnerDriveStudios.com
+ */
+public class SampleScript : MonoBehaviour
+{
+	public int amountOfObjectsToPlace;
+	public GameObject prefabToPlace;
+}
---
+++ TightCity/Assets/Scripts/SampleScriptEditor.cs
@@ -0,0 +1,42 @@
+﻿using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+/**
+ * This is a sample editor window that shows how to generate stuff at editor time.
+ * 
+ * @author J.C. Wichman / InnerDriveStudios.com 
+ */
+
+ [CustomEditor(typeof(SampleScript))]
+public class SampleScriptEditor : Editor
+{
+
+	public override void OnInspectorGUI()
+	{
+		base.OnInspectorGUI();
+
+		SampleScript sampleScript = target as SampleScript;
+	
+		if (GUILayout.Button("Generate"))
+		{
+			if (sampleScript.prefabToPlace != null)
+			{
+				Undo.SetCurrentGroupName("Auto create objects");
+				int group = Undo.GetCurrentGroup();
+
+				for (int i = 0; i < sampleScript.amountOfObjectsToPlace; i++)
+				{
+					GameObject go = Instantiate(sampleScript.prefabToPlace, Random.onUnitSphere * 4, Quaternion.identity);
+
+					Undo.RegisterCreatedObjectUndo(go, "Undo gameobject create");
+				}
+
+				Undo.CollapseUndoOperations(group);
+
+				//important to save the changes
+				EditorSceneManager.MarkSceneDirty(EditorSceneManager.GetActiveScene());
+			}
+		}
+	}
+}
